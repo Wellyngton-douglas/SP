0309a8219740dcc629eb4666b7f6255443733b32
Wellyngton Douglas <wellyngtond159@gmail.com>
2022-09-21 23:05:09 -0300

    customers

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::post('/login','LoginController@autenticar') -> name('site.login');
 Route::middleware('autenticacao:padrao,visitante,p3,p4') -> prefix('/app') -> group(function() {
   Route::get('/home', 'HomeController@index') -> name('app.home');
   Route::get('/sair', 'LoginController@sair') -> name('app.sair');
-  Route::get('/cliente', 'ClientesController@index') -> name('app.cliente');
+  Route::get('/cliente', 'ClienteController@index') -> name('app.cliente');
   Route::get('/fornecedor', 'FornecedorController@index') -> name('app.fornecedor');
   Route::get('/produto', 'ProdutoController@index') -> name('app.produto');
 });
